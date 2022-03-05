18ce07dea3eaa0d666c56518299a9bcd3440aaf0
BuildTools <unconfigured@null.spigotmc.org>
2022-03-05 19:05:38 +0100

    AuthController register,create kesz

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function regsiter(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email'=>'required|string|unique:users,email',
+            'password'=>'required|string|confirmed'
+        ]);
+        $user = User::create([
+            'name'=> $fields['name'],
+            'email'=>$fields['email'],
+            'password'=> bcrypt($fields['password'])
+        ]);
+    }
 }
