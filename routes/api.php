f1caff50e136da73ed27275ffe2a68feb13a9fe3
BuildTools <unconfigured@null.spigotmc.org>
2022-03-05 18:54:10 +0100

    search kesz

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Employee;
+use App\Http\Controllers\EmployeeController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// Route::get('/employees', function(){
+//     return 'employees';
+// });
+// Route::get('/employees', function(){
+//     return Employee::all();
+// });
+// Route::post('/employees',function(){
+//     return Employee::create([
+//         'name' => 'Rágó Ferenc',
+//         'city' => 'Szolnok',
+//         'salary' => 2873000
+//     ]);
+// });
+Route::resource('/employees', EmployeeController::class);
+Route::get('/employees/search/{name}', [EmployeeController::class, 'search']);
+// Route::get('/products',[EmployeeController::class,'index']);
+// Route::post('/employees',[EmployeeController::class,'store']);
+ 
